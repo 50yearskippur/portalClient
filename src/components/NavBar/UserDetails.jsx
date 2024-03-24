f81b710683d6aaab9839837b698a0ce13629b7ac
Harel Dagan <hareldaganh@gmail.com>
2024-03-24 14:36:59 +0200

    resolve conflict in app.css

--- src/components/NavBar/UserDetails.jsx
+++ src/components/NavBar/UserDetails.jsx
@@ -1,13 +1,13 @@
 import profilePicture from "../../assets/media/profilePicture.png";
 import "./UserDetails.css";
 
-const UserDetails = ({ user }) => {
+const UserDetails = ({user}) => {
   return (
     <div className="profile-container">
       <img
         className="profile-picture-icon"
         src={profilePicture}
-        alt="profile picture icon"
+        alt="profile icon"
       />
       <div className="user-info">
         <p className="user-fullName">{user.fullName}</p>
