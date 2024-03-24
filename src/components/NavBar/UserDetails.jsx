a41b3fab07a0f93526d590d6579fe5fd47181566
Alin <12alin.12@gmail.com>
2024-03-24 11:19:38 +0200

    added user details design

--- src/components/NavBar/UserDetails.jsx
+++ src/components/NavBar/UserDetails.jsx
@@ -1,12 +1,18 @@
-import React from "react";
 import profilePicture from "../../assets/media/profilePicture.png";
+import "./UserDetails.css";
 
 const UserDetails = ({ user }) => {
   return (
-    <div>
-      <img src={profilePicture} alt="profile picture icon" />
-      <p>{user.fullName}</p>
-      <p>{user.group}</p>
+    <div className="profile-container">
+      <img
+        className="profile-picture-icon"
+        src={profilePicture}
+        alt="profile picture icon"
+      />
+      <div className="user-info">
+        <p className="user-fullName">{user.fullName}</p>
+        <p className="user-group">{user.group}</p>
+      </div>
     </div>
   );
 };
