9e805c3c56700947af509b55ace910ece00050cd
Alin <12alin.12@gmail.com>
2024-03-26 13:36:35 +0200

    added css file to pdf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
+import FileController from "./components/media/FileController";
+import pdfPage from "./assets/pdfs/pdfPage.pdf";
+
+const items = [{ media: pdfPage, type: "pdf" }];
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <FileController item={items[0]} />
     </div>
   );
 }
--- src/components/media/pdf.jsx
+++ src/components/media/pdf.jsx
@@ -1,6 +1,7 @@
-import {Document, Page} from "react-pdf";
+import { Document, Page } from "react-pdf";
+import "react-pdf/dist/Page/AnnotationLayer.css";
 
-const Pdf = ({file}) => {
+const Pdf = ({ file }) => {
   return (
     <Document file={file} loading={<div>Loading...</div>}>
       <Page pageNumber={1} renderTextLayer={false} width={500} />
