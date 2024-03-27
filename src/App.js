eba638dacd05d541635f28bc9f2f838faba8313f
Harel Dagan <hareldaganh@gmail.com>
2024-03-27 13:24:37 +0200

    fix capitl letter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
+import Favorits from "./components/Favorites/Favorites";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Favorits />
     </div>
   );
 }
