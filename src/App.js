7a07d50771feb61d87be5ef047c97f9bf46e12e2
Harel Dagan <hareldaganh@gmail.com>
2024-03-27 16:20:19 +0200

    in progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
-import Favorites from "./components/Favorites/Favorites";
+import FormPage from "./pages/FormPage";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Favorites />
+      <FormPage />
     </div>
   );
 }
--- src/components/ItemsComponents/PdfPreview.jsx
+++ src/components/ItemsComponents/PdfPreview.jsx
@@ -1,27 +1,27 @@
 import "./PdfPreview.css";
 import FileController from "../Media/FileController";
-import DownloadIcon from "../../../assets/media/icons/downloadIcon.png";
+import DownloadIcon from "../../assets/media/icons/downloadIcon.png";
 
-const downloadPdf = (pdfDetails) => {
+const downloadPdf = (item) => {
   const link = document.createElement("a");
-  link.href = pdfDetails.media;
-  link.download = `${pdfDetails.title}.pdf`; // or any other filename you want
+  link.href = item.media;
+  link.download = `${item.title}.pdf`;
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
 };
 
-const PdfPreview = ({pdfDetails}) => {
+const PdfPreview = ({item}) => {
   return (
     <div className="pdf-preview-container">
-      <FileController item={pdfDetails} />
+      <FileController item={item} style={{width: "300"}} />
       <div className="pdf-preview-info">
         <div>
-          <div className="pdf-preview-title">{pdfDetails.title}</div>
-          <div className="pdf-preview-subject">{pdfDetails.subSubject}</div>
+          <div className="pdf-preview-title">{item.title}</div>
+          <div className="pdf-preview-subject">{item.subSubject.title}</div>
         </div>
         <img
-          onClick={() => downloadPdf(pdfDetails)}
+          onClick={() => downloadPdf(item)}
           className="pdf-preview-download-icon"
           alt="download-icon"
           src={DownloadIcon}
