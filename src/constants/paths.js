8d7e16b5a3bc12e311e751c64be0142a82be4f61
Alin <12alin.12@gmail.com>
2024-04-09 12:45:13 +0300

    fixed bugs development

--- src/constants/paths.js
+++ src/constants/paths.js
@@ -1,7 +1,7 @@
 import Home from "../pages/HomePage";
 import Form from "../pages/FormPage";
 import Media from "../pages/MediaPage";
-import DesignTemplates from "../pages/DesignTemplatesPage";
+import DesignTemplates from "../pages/designTemplatesPage";
 import MediaAlbum from "../pages/MediaAlbumPage";
 
 const paths = [
@@ -20,8 +20,8 @@ const paths = [
     text: "טפסים",
     component: Form,
   },
-  {path: "/media", text: "תמונות ומדיה", component: Media},
-  {path: "/mediaAlbum", text: "אלבומי תמונות", component: MediaAlbum},
+  { path: "/media", text: "תמונות ומדיה", component: Media },
+  { path: "/mediaAlbum", text: "אלבומי תמונות", component: MediaAlbum },
 ];
 
 export default paths;
