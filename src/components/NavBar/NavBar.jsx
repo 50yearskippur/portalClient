8902e9972fdbb921cd22a1daef9216df8d9ca50b
50yearskippur <Simba.artech@gmail.com>
2024-05-26 14:42:22 +0300

    navbar close done

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,51 @@
 import "./NavBar.css";
+import { useNavigate } from "react-router-dom";
+// import closeNavbarIcon from "../../assets/media/Navbar/closeNavbar.svg";
+import openNavbarIcon from "../../assets/media/Navbar/openNavbar.svg";
+import homeIcon from "../../assets/media/Navbar/home.svg";
+import designTemplatesIcon from "../../assets/media/Navbar/designTemplates.svg";
+import formsIcon from "../../assets/media/Navbar/forms.svg";
+import mediaIcon from "../../assets/media/Navbar/media.svg";
+import userIcon from "../../assets/media/Navbar/user.svg";
 
 const NavBar = () => {
-  return <div className="navbar-container"></div>;
+  const navigate = useNavigate();
+  return (
+    <div className="navbar-container">
+      <div className="horizonal-line" />
+      <img src={openNavbarIcon} alt="close navbar icon" />
+      <img
+        src={homeIcon}
+        className="navbar-icon"
+        alt="home icon"
+        onClick={() => navigate("/")}
+      />
+      <img
+        src={designTemplatesIcon}
+        className="navbar-icon"
+        alt="designTemplates icon"
+        onClick={() => navigate("/designTemplates")}
+      />
+      <img
+        src={formsIcon}
+        className="navbar-icon"
+        alt="form icon"
+        onClick={() => navigate("/form")}
+      />
+      <img
+        src={mediaIcon}
+        className="navbar-icon"
+        alt="media icon"
+        onClick={() => navigate("/mediaAlbum")}
+      />
+      <img
+        src={userIcon}
+        className="navbar-icon"
+        alt="media icon"
+        style={{ marginTop: "62.5vh" }}
+      />
+    </div>
+  );
 };
 
 export default NavBar;
