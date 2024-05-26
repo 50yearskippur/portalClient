f494e59433871bc2364e62d2ca363483c70c0dcc
50yearskippur <Simba.artech@gmail.com>
2024-05-26 10:11:26 +0300

    add download function to MediaPopup

--- src/components/Popup/MediaPopup.jsx
+++ src/components/Popup/MediaPopup.jsx
@@ -2,17 +2,25 @@ import "./MediaPopup.css";
 import FileController from "../Media/FileController";
 import backImg from "../../assets/media/Icons/backIcon.svg";
 import downloadImg from "../../assets/media/Icons/downloadIcon.svg";
+import { useContext } from "react";
+import { PopupContext } from "../../store/popup-context";
+import downloadPdf from "../../utils/downloadPdf ";
 
 const MediaPopup = ({ item }) => {
+  const { hidePopup } = useContext(PopupContext);
+
   return (
     <div className="media-popup-container">
       <div className="media-popup-top-container">
-        <div className="media-popup-exit-container">
+        <div className="media-popup-exit-container" onClick={hidePopup}>
           <img src={backImg} alt="back icon" />
           <div className="media-popup-exit-text">יציאה ממצב תצוגה מקדימה</div>
         </div>
         <div className="media-popup-title">{item.title}</div>
-        <div className="media-popup-download-container">
+        <div
+          className="media-popup-download-container"
+          onClick={() => downloadPdf(item)}
+        >
           <img src={downloadImg} alt="download icon" />
           <div className="media-popup-download-text">הורדה</div>
         </div>
