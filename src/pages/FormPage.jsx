c36d28b8d78546ecdb17721e8982faf14c9617be
50yearskippur <138566109+50yearskippur@users.noreply.github.com>
2024-06-10 09:32:35 +0300

    added margin-top to page container

--- src/pages/FormPage.jsx
+++ src/pages/FormPage.jsx
@@ -3,6 +3,7 @@ import PageTemplate from "../components/PageTemplate/PageTemplate";
 import PdfPreview from "../components/ItemsComponents/PdfPreview";
 import Subjects from "../components/Subjects/Subjects";
 import pdf from "../assets/pdfs/pdfPage.pdf";
+import Search from "../components/Search/Search";
 
 const FormPage = () => {
   const data = [
@@ -160,17 +161,22 @@ const FormPage = () => {
       mediaType: "pdf",
     },
   ];
+
   return (
-    <div className="page-container form-page-container">
-      <Subjects />
-      <PageTemplate
+    <div className="page-container">
+      <div className="form-top-page">
+        <div className="header">טפסים</div>
+        <Search style={{ width: "21.17vw", height: "3.38vh" }} />
+      </div>
+      {/* <Subjects /> */}
+      {/* <PageTemplate
         ItemComponent={PdfPreview}
         gridStyle={{
           gridTemplateColumns: `repeat(auto-fill, minmax(250px, 1fr)`,
           gap: "48px",
         }}
         data={data}
-      />
+      /> */}
     </div>
   );
 };
