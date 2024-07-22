50e8a26c4d55ba865dd613f42278b79754cf5b50
50yearskippur <138566109+50yearskippur@users.noreply.github.com>
2024-07-22 12:34:52 +0300

    done design media page

--- src/components/ItemsComponents/CollageMedia.jsx
+++ src/components/ItemsComponents/CollageMedia.jsx
@@ -1,20 +1,42 @@
-import React from 'react';
+import React from "react";
 import "./CollageMedia.css";
 import FileController from "../Media/FileController.jsx";
 import rabit from "../../assets/img/rabit.jpg";
+import usa from "../../assets/img/usa.png";
+import robot from "../../assets/img/robot.jpg";
 import { useNavigate } from "react-router-dom";
 
 const CollageMedia = () => {
   const navigate = useNavigate();
-  const item = {
-    title: "ארנב",
-    type: "חיה",
-    subSubject: {
-      title: "יונקים",
+  const items = [
+    {
+      title: "ארנב",
+      type: "חיה",
+      subSubject: {
+        title: "יונקים",
+      },
+      media: rabit,
+      mediaType: "image",
     },
-    media: rabit,
-    mediaType: "image",
-  };
+    {
+      title: "קיפוד",
+      type: "חיה",
+      subSubject: {
+        title: "יונקים",
+      },
+      media: usa,
+      mediaType: "image",
+    },
+    {
+      title: "קיפוד",
+      type: "חיה",
+      subSubject: {
+        title: "יונקים",
+      },
+      media: robot,
+      mediaType: "image",
+    },
+  ];
   return (
     <div
       className="collage-media-container"
@@ -22,21 +44,21 @@ const CollageMedia = () => {
     >
       <div className="collage-media-image-container">
         <FileController
-          item={item}
+          item={items[0]}
           style={{ borderRadius: "0 10px 10px 0", height: "36vh" }}
         />
         <div className="collage-media-side-container">
           <FileController
-            item={item}
+            item={items[1]}
             style={{ height: "18vh", borderRadius: "10px 0 0 0" }}
           />
           <div className="collage-media-overlay-container">
             <FileController
-              item={item}
+              item={items[2]}
               style={{ height: "18vh", borderRadius: "0 0 0 10px" }}
               gradientStyle={{ background: "#18132B99", height: "100%" }}
             />
-            <div className="collage-media-overlay">3+</div>
+            <div className="collage-media-overlay">{items.length - 2}+</div>
           </div>
         </div>
       </div>
