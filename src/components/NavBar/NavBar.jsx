9538c6e132886aa4518e35746aee69a33bb51b65
50yearskippur <138566109+50yearskippur@users.noreply.github.com>
2024-07-26 16:18:42 +0300

    done design close nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,25 +7,35 @@ import closeNavbarIcon from "../../assets/media/Navbar/closeNavbar.svg";
 import openNavbarIcon from "../../assets/media/Navbar/openNavbar.svg";
 import homeIcon from "../../assets/media/Navbar/home.svg";
 import homeIconSelected from "../../assets/media/Navbar/homeSelected.svg";
-// import designTemplatesIcon from "../../assets/media/Navbar/designTemplates.svg";
-// import designTemplatesIconSelected from "../../assets/media/Navbar/designTemplatesSelected.svg";
 import formsIcon from "../../assets/media/Navbar/forms.svg";
 import formIconSelected from "../../assets/media/Navbar/formsSelected.svg";
 import mediaIcon from "../../assets/media/Navbar/media.svg";
 import mediaIconSelected from "../../assets/media/Navbar/mediaSelected.svg";
 import adminIcon from "../../assets/media/Navbar/admin.svg";
 import adminIconSelected from "../../assets/media/Navbar/adminSelected.svg";
-// import bellIcon from "../../assets/media/Navbar/bell.svg";
+import addPermissionsIcon from "../../assets/media/Navbar/addPermissions.svg";
+import addPermissionsIconSelected from "../../assets/media/Navbar/addPermissionsSelected.svg";
+import telephoneIcon from "../../assets/media/Navbar/telephone.svg";
+// import designTemplatesIcon from "../../assets/media/Navbar/designTemplates.svg";
+// import designTemplatesIconSelected from "../../assets/media/Navbar/designTemplatesSelected.svg";
 
 const NavBar = () => {
   const navigate = useNavigate();
   const location = useLocation();
+
+  const user = {
+    role: "superAdmin",
+  };
+
+  const isAdmin = user.role === "admin" || user.role === "superAdmin";
+  const isSuperAdmin = user.role === "superAdmin";
   const IconsArrays = [
     {
       path: homeIcon,
       selected: homeIconSelected,
       navigateTo: "/",
       text: "עמוד הבית",
+      show: true,
     },
     // {
     //   path: designTemplatesIcon,
@@ -38,19 +48,29 @@ const NavBar = () => {
       selected: formIconSelected,
       navigateTo: "/forms",
       text: "טפסים",
+      show: true,
     },
     {
       path: mediaIcon,
       selected: mediaIconSelected,
       navigateTo: "/media",
       text: "תמונות ומדיה",
+      show: true,
     },
-    // {
-    //   path: adminIcon,
-    //   selected: adminIconSelected,
-    //   navigateTo: "/admin",
-    //   text: "צד אדמין",
-    // },
+    {
+      path: adminIcon,
+      selected: adminIconSelected,
+      navigateTo: "/admin",
+      text: "צד אדמין",
+      show: isAdmin,
+    },
+    {
+      path: addPermissionsIcon,
+      selected: addPermissionsIconSelected,
+      navigateTo: "/permissionsManagement",
+      text: "ניהול הרשאות",
+      show: isSuperAdmin,
+    },
   ];
 
   const [isOpen, setIsOpen] = useState(false);
@@ -84,11 +104,17 @@ const NavBar = () => {
         </div>
         {isOpen && <div className="navbar-portal-text">Portal</div>}
       </div>
-      <div className={`horizonal-line-admin ${isOpen ? "open" : "close"}`} />
+      {isAdmin && (
+        <div className={`horizonal-line-admin ${isOpen ? "open" : "close"}`} />
+      )}
       <div className="navbar-content">
         <div className={`navbar-top-icons ${isOpen && "open"}`}>
           {IconsArrays.map((icon, index) => (
             <div
+              style={{
+                display: !icon.show && "none",
+                marginTop: icon.text === "צד אדמין" && "24px",
+              }}
               key={`icon${index}`}
               className={`navbar-item ${isOpen && "open"} ${
                 chosenIcon?.path === icon.path && "navbar-selected-icon"
@@ -114,7 +140,11 @@ const NavBar = () => {
           ))}
         </div>
         <div className={`navbar-bottom-icons  ${isOpen && "open"}`}>
-          {/* <img src={bellIcon} className="navbar-bell-icon" alt="bell icon" /> */}
+          <img
+            className="navbar-icon"
+            src={telephoneIcon}
+            alt="telephone icon"
+          />
           <PersonalAreaAvater
             name="יכ"
             style={{
