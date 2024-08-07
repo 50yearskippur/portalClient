72fb3d6bfcdfe8cbb90e1cbba65da2c1e43867d7
50yearskippur <138566109+50yearskippur@users.noreply.github.com>
2024-08-07 15:00:10 +0300

    added generic component for admin upload button

--- src/components/Admin/Upload/Settings.jsx
+++ src/components/Admin/Upload/Settings.jsx
@@ -1,27 +1,28 @@
-import { useState, useEffect, useContext } from "react";
-import { PopupContext } from "../../../store/popup-context";
-import { addDays, addMonths, addYears } from "date-fns";
-import SettingsList from "../../Popup/EduPreview/SettingsList";
-import getSettings from "../../../utils/getSettings";
-import Dropdown from "../../Dropdown/Dropdown";
-import levels from "../../../store/levels";
+import { useState, useEffect, useContext } from 'react';
+import { PopupContext } from '../../../store/popup-context';
+import { addDays, addMonths, addYears } from 'date-fns';
+import SettingsList from '../../Popup/EduPreview/SettingsList';
+import getSettings from '../../../utils/getSettings';
+import Dropdown from '../../Dropdown/Dropdown';
+import levels from '../../../store/levels';
 // import SearchDropdown from "../../Search/SearchDropdown";
-import Button from "../../Button/Button";
+import NextBtn from './NextBtn';
 
 const Setting = ({ nextStage }) => {
   const { itemDetails, setItemDetails } = useContext(PopupContext);
-  const [experationDate, setExperationDate] = useState({ num: "", unit: "" });
+  console.log(itemDetails);
+  const [experationDate, setExperationDate] = useState({ num: '', unit: '' });
 
   const calculateFutureDate = (experationDate) => {
     const { unit, num } = experationDate;
     const currentDate = new Date();
 
     switch (unit) {
-      case "ימים":
+      case 'ימים':
         return addDays(currentDate, num);
-      case "חודשים":
+      case 'חודשים':
         return addMonths(currentDate, num);
-      case "שנים":
+      case 'שנים':
         return addYears(currentDate, num);
       default:
         return addMonths(currentDate, num);
@@ -45,22 +46,22 @@ const Setting = ({ nextStage }) => {
           <div className="stage-text">רמה</div>
           <Dropdown
             defaultValue={
-              itemDetails["level"] ? itemDetails["level"] : "בחרו רמת קושי"
+              itemDetails['level'] ? itemDetails['level'] : 'בחרו רמת קושי'
             }
             list={levels}
-            style={{ width: "100%", height: "25px" }}
+            style={{ width: '100%', height: '25px' }}
           />
         </div>
         <div className="stage-input-container">
           <div className="stage-text">זמן מוערך</div>
           <Dropdown
             defaultValue={
-              itemDetails["estimatedTime"]
-                ? itemDetails["estimatedTime"]
+              itemDetails['estimatedTime']
+                ? itemDetails['estimatedTime']
                 : "20 דק'"
             }
             list={["20 דק'", "30 דק'", "45 דק'", "60+ דק'"]}
-            style={{ width: "100%", height: "25px" }}
+            style={{ width: '100%', height: '25px' }}
           />
         </div>
       </div>
@@ -70,26 +71,26 @@ const Setting = ({ nextStage }) => {
           <input
             className="stage-input"
             type="number"
-            style={{ width: "2.969vw", height: "10px" }}
+            style={{ width: '2.969vw', height: '10px' }}
             onChange={(e) =>
               setExperationDate((prevDate) => {
                 return { ...prevDate, num: e.target.value };
               })
             }
             defaultValue={
-              itemDetails["experationDateObject"]?.num
-                ? itemDetails["experationDateObject"].num
+              itemDetails['experationDateObject']?.num
+                ? itemDetails['experationDateObject'].num
                 : 12
             }
           />
           <Dropdown
             defaultValue={
-              itemDetails["experationDateObject"]?.unit
-                ? itemDetails["experationDateObject"].unit
-                : "חודשים"
+              itemDetails['experationDateObject']?.unit
+                ? itemDetails['experationDateObject'].unit
+                : 'חודשים'
             }
-            list={["ימים", "חודשים", "שנים"]}
-            style={{ width: "33.3%", height: "25px" }}
+            list={['ימים', 'חודשים', 'שנים']}
+            style={{ width: '33.3%', height: '25px' }}
             onSelect={(item) =>
               setExperationDate((prevDate) => {
                 return { ...prevDate, unit: item };
@@ -135,19 +136,14 @@ const Setting = ({ nextStage }) => {
           fieldName="tags"
         />
       </div> */}
-      <Button
-        text={"העלה תוצר"}
-        style={{
-          width: "6.667vw",
-          height: "4.4vh",
-          margin: "0 auto",
-        }}
+      <NextBtn
+        text={'העלה תוצר'}
         disabled={
-          !itemDetails["title"] ||
-          !itemDetails["subject"] ||
-          !itemDetails["subSubject"]
+          !itemDetails['level'] ||
+          !itemDetails['estimatedTime'] ||
+          !itemDetails['experationDate']
         }
-        onClick={nextStage}
+        nextStage={nextStage}
       />
     </div>
   );
