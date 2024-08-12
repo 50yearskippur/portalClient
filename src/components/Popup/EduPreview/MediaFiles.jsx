2c112e81ac74b2aa7e95eff931d46c9a5f928cbb
50yearskippur <138566109+50yearskippur@users.noreply.github.com>
2024-08-12 10:38:01 +0300

    changed names of files

--- src/components/Popup/EduPreview/MediaFiles.jsx
+++ src/components/Popup/EduPreview/MediaFiles.jsx
@@ -1,6 +1,5 @@
-import React from "react";
-import "./FileGrid.css"; // Import the CSS file
-import FileController from "../../Media/FileController";
+import './MediaFiles.css'; 
+import FileController from '../../Media/FileController';
 
 const ImageGrid = ({ edu }) => {
   const images = edu.files;
@@ -10,16 +9,16 @@ const ImageGrid = ({ edu }) => {
         <div className="cover-container">
           <div className="cover-title">תמונת קאבר</div>
           <FileController
-            item={{ media: edu.cover, mediaType: "image" }}
-            style={{ width: "100%", height: "100%", borderRadius: "8px" }}
+            item={{ media: edu.cover, mediaType: 'image' }}
+            style={{ width: '100%', height: '100%', borderRadius: '8px' }}
           />
         </div>
         <div className="image-grid">
           {images.map((src, index) => (
             <div className="grid-item" key={index}>
               <FileController
-                item={{ media: src, mediaType: "image" }}
-                style={{ height: "100%" }}
+                item={{ media: src, mediaType: 'image' }}
+                style={{ height: '100%' }}
               />
             </div>
           ))}
