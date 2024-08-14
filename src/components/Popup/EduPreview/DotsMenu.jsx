5f892b49e1d8f8fc682d2ffd49c3d409db1e6051
50yearskippur <138566109+50yearskippur@users.noreply.github.com>
2024-08-14 12:55:36 +0300

    refactor: Remove unused SVG files and update DotsMenu styling

--- src/components/Popup/EduPreview/DotsMenu.jsx
+++ src/components/Popup/EduPreview/DotsMenu.jsx
@@ -1,29 +1,42 @@
 import './DotsMenu.css';
 import { useState } from 'react';
-import whiteDot from '../../../assets/media/Icons/whiteDot.svg';
 import redTrash from '../../../assets/media/Icons/redTrash.svg';
 import back from '../../../assets/media/Icons/back.svg';
 
-const DotsMenu = () => {
+const DotsMenu = ({
+  handleDeleteImage,
+  handleRotateImage,
+  currentImage,
+}) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuHover = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
 
   return (
     <>
       <div
         className="dots-container"
-        onClick={() => setIsMenuOpen((prev) => !prev)}
+        onMouseEnter={() => handleMenuHover(currentImage)}
       >
         {Array.from({ length: 3 }).map(() => (
           <div className="dot" />
         ))}
       </div>
       {isMenuOpen && (
-        <div className="media-setting-popup">
-          <div className="media-setting-row">
+        <div className="media-setting-popup" onMouseLeave={handleMenuHover}>
+          <div
+            className="media-setting-row"
+            onClick={() => handleRotateImage(currentImage)}
+          >
             <img src={back} alt="back" />
             <div className="media-setting-rotation">סיבוב תמונה</div>
           </div>
-          <div className="media-setting-row">
+          <div
+            className="media-setting-row"
+            onClick={() => handleDeleteImage(currentImage)}
+          >
             <img src={redTrash} alt="trash" />
             <div className="media-setting-delete">מחיקה</div>
           </div>
--- src/components/Popup/EduPreview/MediaFilesPreview.jsx
+++ src/components/Popup/EduPreview/MediaFilesPreview.jsx
@@ -5,7 +5,46 @@ import { PopupContext } from '../../../store/popup-context';
 import grayPlus from '../../../assets/media/Icons/grayPlus.svg';
 import DotsMenu from './DotsMenu';
 
-const BigImage = ({ image, onDrop }) => {
+const rotateImageOnCanvas = (imageFile, callback) => {
+  console.log('in');
+  console.log(imageFile);
+  const reader = new FileReader();
+  reader.readAsDataURL(imageFile);
+
+  reader.onload = function (event) {
+    const img = new Image();
+    img.src = event.target.result;
+
+    img.onload = function () {
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d');
+
+      // Set canvas size to the dimensions of the image, swapping width and height for 90-degree rotation
+      canvas.width = img.height;
+      canvas.height = img.width;
+
+      // Translate canvas context to the center of the canvas
+      ctx.translate(canvas.width / 2, canvas.height / 2);
+
+      // Rotate the canvas 90 degrees
+      ctx.rotate((90 * Math.PI) / 180);
+
+      // Draw the image onto the canvas, adjusting position based on rotation
+      ctx.drawImage(img, -img.width / 2, -img.height / 2);
+
+      // Convert the canvas back to a file object
+      canvas.toBlob((blob) => {
+        const rotatedFile = new File([blob], imageFile.name, {
+          type: imageFile.type,
+          lastModified: Date.now(),
+        });
+        callback(rotatedFile);
+      });
+    };
+  };
+};
+
+const BigImage = ({ image, onDrop, handleDeleteImage, handleRotateImage }) => {
   const [{ isOver }, drop] = useDrop({
     accept: 'image',
     drop: (item) => onDrop(item),
@@ -27,12 +66,22 @@ const BigImage = ({ image, onDrop }) => {
         alt="file"
         className="media-file-cover grab"
       />
-      <DotsMenu />
+      <DotsMenu
+        handleDeleteImage={handleDeleteImage}
+        handleRotateImage={handleRotateImage}
+        currentImage={image}
+      />
     </div>
   );
 };
 
-const SmallImage = ({ image, index, setCurrentImageSwap }) => {
+const SmallImage = ({
+  image,
+  index,
+  setCurrentImageSwap,
+  handleDeleteImage,
+  handleRotateImage,
+}) => {
   const [{ isDragging }, drag] = useDrag({
     type: 'image',
     item: { index, image },
@@ -56,13 +105,18 @@ const SmallImage = ({ image, index, setCurrentImageSwap }) => {
         alt="file"
         className="media-file grabbing"
       />
-      <DotsMenu />
+      <DotsMenu
+        handleDeleteImage={handleDeleteImage}
+        handleRotateImage={handleRotateImage}
+        currentImage={image}
+      />
     </div>
   );
 };
 
 const MediaFilesPreview = ({ files }) => {
-  const { setItemDetails } = useContext(PopupContext);
+  const { setItemDetails, itemDetails } = useContext(PopupContext);
+  console.log(itemDetails);
   const [images, setImages] = useState(files);
   const [currentImageSwap, setCurrentImageSwap] = useState();
   const fileInputRef = useRef(null);
@@ -98,13 +152,36 @@ const MediaFilesPreview = ({ files }) => {
     setImages((prevImages) => [...prevImages, ...newImages]);
   };
 
+  const handleDeleteImage = (imgaeToDelete) => {
+    const newImages = images.filter(
+      (image) => image.path !== imgaeToDelete.path
+    );
+    setImages(newImages);
+  };
+
+  const handleRotateImage = (file) => {
+    console.log('handleRotateImage');
+    rotateImageOnCanvas(file, (rotatedFile) => {
+      const updatedImages = images.map((image) =>
+        image.path === rotatedFile.path ? rotatedFile : image
+      );
+
+      setImages(updatedImages);
+    });
+  };
+
   const triggerFileInput = () => {
     fileInputRef.current.click();
   };
 
   return (
     <div className="media-files-preview-containr">
-      <BigImage image={images[0]} onDrop={() => onDrop(currentImageSwap)} />
+      <BigImage
+        image={images[0]}
+        onDrop={() => onDrop(currentImageSwap)}
+        handleDeleteImage={handleDeleteImage}
+        handleRotateImage={handleRotateImage}
+      />
       <div className="media-files-grid">
         {images.slice(1).map((image, index) => (
           <>
@@ -112,6 +189,8 @@ const MediaFilesPreview = ({ files }) => {
               key={index}
               image={image}
               setCurrentImageSwap={setCurrentImageSwap}
+              handleDeleteImage={handleDeleteImage}
+              handleRotateImage={handleRotateImage}
             />
             <DotsMenu />
           </>
