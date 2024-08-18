a2fda0b60df5ff2c181b545add3607942b2b169c
yaelaflalo <aflaloyael@gmail.com>
2024-08-18 15:23:12 +0300

    fix conflicts

--- src/pages/SubSubjectPage.jsx
+++ src/pages/SubSubjectPage.jsx
@@ -1,122 +1,77 @@
-import React from "react";
-import "./SubSubjectPage.css";
-import { useLocation } from "react-router-dom";
-import { useContext } from "react";
-import TopSection from "../components/TopSection/TopSection";
-import FileController from "../components/Media/FileController";
-import SideBar from "../components/SideBar/SideBar";
-import Rate from "../components/Rate/Rate";
-import EduResPrev from "../components/EduResPrev/EduResPrev";
-import UploadEduType from "../components/Popup/UploadEdu/UploadEduType";
-import { PopupContext } from "../store/popup-context";
-import notebookImg from "../assets/media/Icons/notebook.svg";
-import heartimg from "../assets/media/Icons/fullHeart.svg";
-import plusImg from "../assets/media/Icons/plus.svg";
-import point from "../assets/media/Icons/pinkPoint.svg";
-import headpones from "../assets/media/Icons/headphones.svg";
-import clock from "../assets/media/Icons/clock.svg";
-import bluePlus from "../assets/media/Icons/bluePlus.svg";
+import React, { useContext, useState } from 'react';
+import './SubSubjectPage.css';
+import { useLocation, useNavigate } from 'react-router-dom';
+import TopSection from '../components/TopSection/TopSection';
+import FileController from '../components/Media/FileController';
+import SideBar from '../components/SideBar/SideBar';
+import Rate from '../components/Rate/Rate';
+import EduResPreview from '../components/EduResPreview/EduResPreview';
+import UploadEduType from '../components/Popup/UploadEdu/UploadEduType';
+import { PopupContext } from '../store/popup-context';
+import notebookImg from '../assets/media/Icons/notebook.svg';
+import heartimg from '../assets/media/Icons/fullHeart.svg';
+import point from '../assets/media/Icons/pinkPoint.svg';
+import headphones from '../assets/media/Icons/headphones.svg';
+import headphonesSilent from '../assets/media/Icons/headphonesSilent.svg';
+import clock from '../assets/media/Icons/clock.svg';
+import addedToFavoritesIcon from '../assets/media/Icons/addedToFavoritesIcon.svg';
 // delete in production
-import rabit from "../assets/img/rabit.jpg";
+import bluePlus from '../assets/media/Icons/bluePlus.svg';
+import convertDateToString from '../utils/convertDateToString';
+// import plusImg from "../assets/media/Icons/plus.svg";
 
 const SubSubjectPage = () => {
   const { showPopup } = useContext(PopupContext);
   const [isAddedToFavorites, setIsAddedToFavorites] = useState(false);
   const location = useLocation();
-  const currentEdu = location.state?.item;
+  const navigate = useNavigate();
+  const subSubject = location.state?.subSubject;
+  const currentEdu = location.state?.item || subSubject?.eduResource[0];
+  const uploadByAdmin = [];
+  const uploadByNonAdmin = [];
 
-  //delete in production
-  const eduResources = {
-    uploadByArtch: [
-      {
-        title: "אמצעי למידה",
-        type: "סרטון",
-        date: Date("28.8.2024"),
-        subSubject: {
-          title: "זירת במבה",
-        },
-        subject: { title: "מבואות מודיעין" },
-        media: rabit,
-        mediaType: "image",
-        level: "רמה בסיסית",
-        uploadByArtech: true,
-      },
-      {
-        title: "אמצעי למידה",
-        type: "סרטון",
-        date: Date("28.05.2024"),
-        subSubject: {
-          title: "זירת במבה",
-        },
-        subject: { title: "מבואות מודיעין" },
-        media: rabit,
-        mediaType: "image",
-        level: "רמה בסיסית",
-        uploadByArtech: true,
-      },
-      {
-        title: "אמצעי למידה",
-        type: "סרטון",
-        date: Date("28.8.2024"),
-        subSubject: {
-          title: "זירת במבה",
-        },
-        subject: { title: "מבואות מודיעין" },
-        media: rabit,
-        mediaType: "image",
-        level: "רמה בסיסית",
-        uploadByArtech: true,
-      },
-    ],
-    uploadByOther: [
-      {
-        title: "במבה מאז ועד היום",
-        type: "סרטון",
-        date: Date("28.8.2024"),
-        subSubject: {
-          title: "זירת במבה",
-        },
-        subject: { title: "מבואות מודיעין" },
-        media: rabit,
-        mediaType: "image",
-        level: "מותאם לכל הרמות",
-        uploadByArtech: false,
-      },
-      {
-        title: "במבה מאז ועד היום",
-        type: "סרטון",
-        date: Date("28.8.2024"),
-        subSubject: {
-          title: "זירת במבה",
-        },
-        subject: { title: "מבואות מודיעין" },
-        media: rabit,
-        mediaType: "image",
-        level: "מותאם לכל הרמות",
-        uploadByArtech: false,
-      },
-      {
-        title: "במבה מאז ועד היום",
-        type: "סרטון",
-        date: Date("28.8.2024"),
-        subSubject: {
-          title: "זירת במבה",
-        },
-        subject: { title: "מבואות מודיעין" },
-        media: rabit,
-        mediaType: "image",
-        level: "מותאם לכל הרמות",
-        uploadByArtech: false,
-      },
-    ],
+  subSubject?.eduResource?.forEach((eduResource) => {
+    if (eduResource.creator.isAdmin) {
+      uploadByAdmin.push(eduResource);
+    } else {
+      uploadByNonAdmin.push(eduResource);
+    }
+  });
+
+  const handleItemClick = (item) => {
+    navigate(location.pathname, { state: { subSubject, item } });
+  };
+
+  const onAddToFavoritesHandler = () => {
+    setIsAddedToFavorites((prev) => !prev);
+  };
+
+  const getEduCredits = (credits) => {
+    return (
+      <div className="edu-resource-cradit-container">
+        {credits.map((credit, index) => {
+          return (
+            <div className="edu-resource-cradit" key={index}>
+              <div className="edu-resource-text" style={{ color: '#7D81B2' }}>
+                {credit.role}
+              </div>
+              <div className="edu-resource-text">{credit.user.fullName}</div>
+            </div>
+          );
+        })}
+      </div>
+    );
   };
 
   return (
     <>
-      <TopSection title={currentEdu?.subSubject?.title} navigateTo={"/"} />
+      <TopSection title={subSubject.title} navigateTo={'/'} />
       <div className="page-container edu-resource-page">
         <div className="edu-resource-container">
-          <FileController item={currentEdu} style={{ height: "54.1vh" }} />
+          <FileController
+            item={currentEdu.files}
+            style={{ height: '54.1vh' }}
+          />
           <div className="edu-resource-title-container">
             <div className="edu-resource-info-container">
               <img
@@ -134,8 +89,9 @@ const SubSubjectPage = () => {
                 className="edu-resource-add-container"
                 onClick={onAddToFavoritesHandler}
                 style={{
-                  border: "1.5px solid #FC4E84",
-                  color: "#FC4E84",
+                  border: '1.5px solid #FC4E84',
+                  color: isAddedToFavorites ? 'white' : '#FC4E84',
+                  backgroundColor: isAddedToFavorites ? '#FC4E84' : 'white',
                 }}
               >
                 <img
@@ -196,49 +152,14 @@ const SubSubjectPage = () => {
           <div className="horizontal-line" />
           <div className="edu-resource-comments">
             <div className="edu-resource-comment-text">
-              אהבתם את הכרת
-              <span style={{ fontWeight: "700" }}> זירת במבה </span>
+              אהבתם את
+              <span style={{ fontWeight: '700' }}> {currentEdu.title}? </span>
               יש לכם הערות? נשמח לשמוע את דעתכם ולהשתפר!
             </div>
             <Rate style={{ width: '48px' }} />
           </div>
-          <div className="edu-resourse-text">
-            משחק שנועד ללמד את צבעי הכומתות בחיילות השונים משחק שנועד ללמד את
-            צבעי הכומתות בחיילות השונים משחק שנועד ללמד את צבעי הכומתות בחיילות
-            השונים
-          </div>
-          <div className="edu-resourse-cradit-container">
-            <div className="edu-resourse-cradit">
-              <div className="edu-resourse-text" style={{ color: "#7D81B2" }}>
-                פרויקטור
-              </div>
-              <div className="edu-resourse-text">שם פרויקטור</div>
-            </div>
-            <div className="edu-resourse-cradit">
-              <div className="edu-resourse-text" style={{ color: "#7D81B2" }}>
-                מומחה תוכן
-              </div>
-              <div className="edu-resourse-text">שם מומחה תוכן</div>
-            </div>
-            <div className="edu-resourse-cradit">
-              <div className="edu-resourse-text" style={{ color: "#7D81B2" }}>
-                עיצוב גרפי
-              </div>
-              <div className="edu-resourse-text">שם עיצוב גרפי</div>
-            </div>
-            <div className="edu-resourse-cradit">
-              <div className="edu-resourse-text" style={{ color: "#7D81B2" }}>
-                פרויקטור
-              </div>
-              <div className="edu-resourse-text">שם פיתוח</div>
-            </div>
-            <div className="edu-resourse-cradit">
-              <div className="edu-resourse-text" style={{ color: "#7D81B2" }}>
-                פרויקטור
-              </div>
-              <div className="edu-resourse-text">שם פיצוח תוכן</div>
-            </div>
-          </div>
+          <div className="edu-resource-text">{currentEdu.description}</div>
+          {getEduCredits(currentEdu.credits)}
         </div>
         <div className="edu-resource-other-container">
           <div className="edu-resource-other-users">
@@ -248,9 +169,10 @@ const SubSubjectPage = () => {
               propsToItemComponent={{ isUploadByAdmin: true }}
               numItems={3}
               startFrom={0}
-              ItemComponent={EduResPrev}
-              data={eduResources?.uploadByArtch}
-              style={{ height: "30.4vh" }}
+              ItemComponent={EduResPreview}
+              data={uploadByAdmin}
+              style={{ height: '30.4vh' }}
+              onItemClick={handleItemClick}
             />
           </div>
           <div
@@ -261,8 +183,8 @@ const SubSubjectPage = () => {
             <div className="edu-resource-add-edu">
               <div className="edu-resource-add-text" style={{ width: '13vw' }}>
                 יש לכם חומר על
-                <span style={{ fontWeight: "700" }}> זירת במבה </span>? שתפו
-                אותנו ואולי תופיעו בפורטל
+                <span style={{ fontWeight: '700' }}> {subSubject.title}</span>?
+                שתפו אותנו ואולי תופיעו בפורטל
               </div>
               <div
                 className="edu-resource-add-container"
@@ -284,9 +206,11 @@ const SubSubjectPage = () => {
               isWithShadow={true}
               numItems={3}
               startFrom={0}
-              ItemComponent={EduResPrev}
-              data={eduResources?.uploadByOther}
-              style={{ height: "30.4vh" }}
+              ItemComponent={EduResPreview}
+              propsToItemComponent={{ isUploadByAdmin: false }}
+              data={uploadByNonAdmin}
+              style={{ height: '30.4vh' }}
+              onItemClick={handleItemClick}
             />
           </div>
         </div>
