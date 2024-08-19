e429c50cde9f8001b5ff57ffb7d07872f6b516c2
yaelaflalo <aflaloyael@gmail.com>
2024-08-19 16:18:48 +0300

    add row

--- src/utils/getAdminChildren.js
+++ src/utils/getAdminChildren.js
@@ -1,20 +1,24 @@
-import EduResourceRow from "../components/Admin/RowTypes/EduResourceRow";
-import DesignTemplateRow from "../components/Admin/RowTypes/DesignTemplateRow";
-import FormRow from "../components/Admin/RowTypes/FormRow";
-import adminRow from "../components/Admin/RowTypes/adminRow";
-import MediaRow from "../components/Admin/RowTypes/MediaRow";
+import EduResourceRow from '../components/Admin/RowTypes/EduResourceRow';
+import DesignTemplateRow from '../components/Admin/RowTypes/DesignTemplateRow';
+import FormRow from '../components/Admin/RowTypes/FormRow';
+import adminRow from '../components/Admin/RowTypes/adminRow';
+import MediaRow from '../components/Admin/RowTypes/MediaRow';
+import AdminSubsubjectsRow from '../components/Admin/RowTypes/AdminSubsubjectsRow';
+
 const getAdminChildren = (item, selectedTab) => {
   switch (true) {
-    case selectedTab?.includes("תוצרים"):
+    case selectedTab?.includes('תוצרים'):
       return EduResourceRow(item);
-    case selectedTab?.includes("עיצובים"):
+    case selectedTab?.includes('עיצובים'):
       return DesignTemplateRow(item);
-    case selectedTab?.includes("טפסים"):
+    case selectedTab?.includes('טפסים'):
       return FormRow(item);
-    case selectedTab?.includes("תמונות סטוק ואלבומים"):
+    case selectedTab?.includes('תמונות סטוק ואלבומים'):
       return MediaRow(item);
-    case selectedTab?.includes("אדמינים"):
+    case selectedTab?.includes('אדמינים'):
       return adminRow(item);
+    case selectedTab?.includes('תתי נושאים'):
+      return AdminSubsubjectsRow(item);
     default:
       return EduResourceRow(item);
   }
