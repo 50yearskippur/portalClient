7335041eda5d543db35d2b20d5646105b796d964
yaelaflalo <aflaloyael@gmail.com>
2024-08-20 13:06:26 +0300

    add icon style option

--- src/components/Admin/RowTypes/TemplateRow.jsx
+++ src/components/Admin/RowTypes/TemplateRow.jsx
@@ -3,21 +3,28 @@ import React from 'react';
 import { PopupContext } from '../../../store/popup-context';
 import iconType from '../RowTypes/IconType';
 
-const TemplateRow = React.memo(({ children, item, PopUp, onClick, icon }) => {
-  const { showPopup } = useContext(PopupContext);
-  return (
-    <div
-      style={{ backgroundColor: iconType(item)[1]?.color }}
-      className="generic-row-container"
-      onClick={onClick ? onClick : () => showPopup(<PopUp edu={item} />)}
-    >
-      {children.map((child, index) => (
-        <div key={`child ${index}`} className="generic-row-child">
-          {child}
-        </div>
-      ))}
-      <img src={icon} alt="arrow" className="generic-row-arrow" />
-    </div>
-  );
-});
+const TemplateRow = React.memo(
+  ({ children, item, PopUp, onClick, icon, iconStyle }) => {
+    const { showPopup } = useContext(PopupContext);
+    return (
+      <div
+        style={{ backgroundColor: iconType(item)[1]?.color }}
+        className="generic-row-container"
+        onClick={onClick ? onClick : () => showPopup(<PopUp edu={item} />)}
+      >
+        {children.map((child, index) => (
+          <div key={`child ${index}`} className="generic-row-child">
+            {child}
+          </div>
+        ))}
+        <img
+          src={icon}
+          alt="arrow"
+          className="generic-row-arrow"
+          style={iconStyle}
+        />
+      </div>
+    );
+  }
+);
 export default TemplateRow;
