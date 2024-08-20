2bcbc8c5816dbecddbfe8e5bd0048617b0be480b
yaelaflalo <aflaloyael@gmail.com>
2024-08-20 09:46:02 +0300

    format code

--- src/components/Admin/RowTypes/TemplateRow.jsx
+++ src/components/Admin/RowTypes/TemplateRow.jsx
@@ -1,7 +1,7 @@
-import { useContext } from "react";
-import React from "react";
-import { PopupContext } from "../../../store/popup-context";
-import iconType from "../RowTypes/IconType";
+import { useContext } from 'react';
+import React from 'react';
+import { PopupContext } from '../../../store/popup-context';
+import iconType from '../RowTypes/IconType';
 
 const TemplateRow = React.memo(({ children, item, PopUp, onClick, icon }) => {
   const { showPopup } = useContext(PopupContext);
