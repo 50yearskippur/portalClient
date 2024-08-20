07273f1cba514e5bebcdf1aae4929e0f262559ef
yaelaflalo <aflaloyael@gmail.com>
2024-08-20 08:39:22 +0300

    replace personal avater to be temporary subsubject image

--- src/components/Admin/RowTypes/AdminSubsubjectsRow.jsx
+++ src/components/Admin/RowTypes/AdminSubsubjectsRow.jsx
@@ -1,20 +1,18 @@
 import './TemplateRow.css';
 import GenericItem from '../GenericObjects/GenericItem';
 import deleteIcon from '../../../assets/media/Icons/exitIcon.svg';
+import temporaryImage from '../../../assets/img/party.png';
 
-import PersonalAreaAvater from '../../PersonalAreaAvater/PersonalAreaAvater';
 const AdminSubsubjectsRow = (item) => {
   return [
-    <PersonalAreaAvater
-      style={{
-        width: '40px',
-        height: '40px',
-        fontSize: '14px',
-        borderRadius: '43px',
-      }}
-      name={item.personalAreaName}
-    />,
-    <GenericItem title={item.subjectName} style={{ width: '8.3vw' }} />,
+    <div className="generic-row-item-container">
+      <img
+        src={temporaryImage}
+        alt="subsubject image"
+        className="subsubject-img"
+      />
+      <GenericItem title={item.subjectName} style={{ width: '8.3vw' }} />
+    </div>,
     <div className="subsubject-row-tag-container">
       {item?.subSubjectTags?.map((tag, tagIndex) => (
         <div key={`tag ${tagIndex}`} className="subsubject-row-tag">
