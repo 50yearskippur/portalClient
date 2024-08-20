d77ab76577938174242b3f3d105e21cc2b1f232a
yaelaflalo <aflaloyael@gmail.com>
2024-08-20 14:12:34 +0300

    add search option

--- src/components/Popup/AddTag/AddTagPopup.jsx
+++ src/components/Popup/AddTag/AddTagPopup.jsx
@@ -1,5 +1,26 @@
-const MediaPreview = () => {
-  return <div></div>;
+import './AddTagPopup.css';
+import Search from '../../Search/Search';
+
+//TODO: consider changing the prop name
+const MediaPreview = ({ edu }) => {
+  return (
+    <div>
+      <div className="popup-stage-white-container">
+        {/* {edu.name} */}
+        <div className="popup-title">תתי נושא רלוונטים</div>
+        <Search
+          placeholder={'חיפוש תתי נושא'}
+          style={{
+            height: '42px',
+            width: '16.7vw',
+            borderRadius: '4px',
+            width: '460px',
+            height: '40px',
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default MediaPreview;
