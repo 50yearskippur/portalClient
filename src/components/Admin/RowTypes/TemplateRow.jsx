8ad430f3f30da22ffb7e7a51f6ce8937bc8c28cf
yaelaflalo <aflaloyael@gmail.com>
2024-08-22 11:27:17 +0300

    add condition. just if icon = true, shows the icon

--- src/components/Admin/RowTypes/TemplateRow.jsx
+++ src/components/Admin/RowTypes/TemplateRow.jsx
@@ -3,28 +3,23 @@ import React from 'react';
 import { PopupContext } from '../../../store/popup-context';
 import iconType from '../RowTypes/IconType';
 
-const TemplateRow = React.memo(
-  ({ children, item, PopUp, onClick, icon, iconStyle }) => {
-    const { showPopup } = useContext(PopupContext);
-    return (
-      <div
-        style={{ backgroundColor: iconType(item)[1]?.color }}
-        className="generic-row-container"
-        onClick={onClick ? onClick : () => showPopup(<PopUp edu={item} />)}
-      >
-        {children.map((child, index) => (
-          <div key={`child ${index}`} className="generic-row-child">
-            {child}
-          </div>
-        ))}
-        <img
-          src={icon}
-          alt="arrow"
-          className="generic-row-arrow"
-          style={iconStyle}
-        />
-      </div>
-    );
-  }
-);
+const TemplateRow = React.memo(({ children, item, PopUp, onClick, icon }) => {
+  const { showPopup } = useContext(PopupContext);
+
+  return (
+    <div
+      style={{ backgroundColor: iconType(item)[1]?.color }}
+      className="generic-row-container"
+      onClick={onClick ? onClick : () => showPopup(<PopUp edu={item} />)}
+    >
+      {children.map((child, index) => (
+        <div key={`child ${index}`} className="generic-row-child">
+          {child}
+        </div>
+      ))}
+
+      {icon && <img src={icon} alt="arrow" className="generic-row-arrow" />}
+    </div>
+  );
+});
 export default TemplateRow;
