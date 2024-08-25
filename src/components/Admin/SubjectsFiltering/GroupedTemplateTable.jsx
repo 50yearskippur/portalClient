8362034b7676f4abf290fb1b6f3c3f3f959a5fcd
yaelaflalo <aflaloyael@gmail.com>
2024-08-25 11:08:08 +0300

    add title up to the subsubjects with the same subject name

---
+++ src/components/Admin/SubjectsFiltering/GroupedTemplateTable.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import TemplateTable from '../GenericObjects/TemplateTable';
+
+const GroupedTemplateTable = ({ data, selectedTab }) => {
+
+    const groupedData = data.reduce((acc, item) => {
+    const { subjectName } = item;
+    if (!acc[subjectName]) {
+      acc[subjectName] = [];
+    }
+    acc[subjectName].push(item);
+    return acc;
+  }, {});
+
+  return (
+    <div className="grouped-template-table">
+      {Object.keys(groupedData).map((subjectName) => (
+        <div key={subjectName} className="grouped-section">
+          <div className="grouped-section-title">{subjectName}</div>
+          <TemplateTable
+            dataArr={groupedData[subjectName]}
+            selectedTab={selectedTab}
+            onClick={() => {}}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default GroupedTemplateTable;
